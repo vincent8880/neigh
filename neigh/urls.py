9788e2027a96fe656a29bf37d42e2b623a38c06b
Vincent <ododovincent54@gmail.com>
2019-08-14 12:49:21 +0300

    updated hood urls.py to set up connections to my views.py

--- neigh/urls.py
+++ neigh/urls.py
@@ -1,4 +1,4 @@
-"""neigh URL Configuration
+"""area URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'',include('hood.urls')),
+    url(r'^accounts/',include('registration.backends.simple.urls')),
+    url(r'^logout/$',views.logout, {"next_page":'/'},name="logout"),
+    url(r'^tinymce',include('tinymce.urls')),
 ]
